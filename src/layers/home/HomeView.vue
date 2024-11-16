0d014ba25e2b7cb9b474bd28923cea916f7ff314
chirbard <matamm@outlook.com>
2024-11-16 14:57:41 +0200

    feat: simple prompting works

--- src/layers/home/HomeView.vue
+++ src/layers/home/HomeView.vue
@@ -2,7 +2,13 @@
   <div>
     <h1>Home</h1>
     <p>Welcome to the Home page!</p>
+    <p>last request: {{ lastRequest }}</p>
+    <p>response: {{ response }}</p>
+
+
+    <input type="text" v-model="input" />
     <button @click="request">make request</button>
+
   </div>
 </template>
 
--- src/layers/home/home-script.ts
+++ src/layers/home/home-script.ts
@@ -7,8 +7,18 @@ export default defineComponent({
   setup() {
     return {};
   },
+  data() {
+    return {
+      input: "",
+      lastRequest: "",
+      response: "",
+    };
+  },
   methods: {
     request() {
+      this.lastRequest = this.input;
+      this.input = "";
+
       const requestBody: string = `{
         "model_id": "meta-llama/llama-3-8b-instruct",
         "project_id": "${import.meta.env.VITE_PROJECT_ID}",
@@ -22,25 +32,12 @@ export default defineComponent({
             "content": [
               {
                 "type": "text",
-                "text": "Who won the world series in 2020?"
-              }
-            ]
-          },
-          {
-            "role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020."
-          },
-          {
-            "role": "user",
-            "content": [
-              {
-                "type": "text",
-                "text": "Where was it played?"
+                "text": "${this.lastRequest}"
               }
             ]
           }
         ],
-        "max_tokens": 100,
+        "max_tokens": 30,
         "temperature": 0,
         "time_limit": 1000
       }`;
@@ -51,8 +48,11 @@ export default defineComponent({
           "https://eu-de.ml.cloud.ibm.com/ml/v1/text/chat?version=2023-10-25",
           requestBody
         )
-        .then((response) => {
+        .then(async (response) => {
           console.log(response);
+          const json = await response.json();
+          this.response = json.choices[0].message.content;
+          console.log(json);
         })
         .catch((error) => {
           console.error(error);
